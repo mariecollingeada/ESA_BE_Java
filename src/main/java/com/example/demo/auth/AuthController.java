6a863fa3b8ad903f930a19cf17bb0ebc09fc5f26
marie.collinge <marie.collinge@monygroup.com>
2026-03-16 13:28:17 +0000

    fix: linting

--- src/main/java/com/example/demo/auth/AuthController.java
+++ src/main/java/com/example/demo/auth/AuthController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
