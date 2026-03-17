1888c8483fc4674147d592fd4dadaf1ee18285b2
marie.collinge <marie.collinge@monygroup.com>
2026-03-17 11:44:34 +0000

    fix: try to get round cors issue

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.cors(Customizer.withDefaults())
-        .csrf(csrf -> csrf.ignoringRequestMatchers("/auth/**"))
+        .csrf(csrf -> csrf.disable())
         .httpBasic(b -> b.disable())
         .authorizeHttpRequests(
             auth ->
